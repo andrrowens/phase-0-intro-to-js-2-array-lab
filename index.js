f8afc2cfc49fb464b5e4e19d58d51b8199205935
Andrew Owens <andrrowens@gmail.com>
2022-11-26 19:23:56 -0700

    commit tree 3dfa2e1f5799e2b01a84effefc3d45a5679ea69b

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+  name = "Ralph";
+  cats.push(name);
+  return cats;
+}
+
+function destructivelyPrependCat(name) {
+  name = "Bob";
+  cats.unshift(name);
+  return cats;
+}
+
+function destructivelyRemoveLastCat(name) {
+  cats.pop();
+  return cats;
+}
+
+function destructivelyRemoveFirstCat(name) {
+  cats.shift();
+  return cats;
+}
+
+function appendCat(name) {
+    let newCats = [...cats, "Broom"]
+    return newCats
+}
+
+function prependCat(name) {
+    let catsRevised = ["Arnold", ...cats]
+    return catsRevised
+}
+
+function removeLastCat() {
+    const newKitties = cats.slice(0, 2)
+    return newKitties
+}
+
+function removeFirstCat() {
+    const updatedCats = cats.slice(1)
+    return updatedCats
+}
+
+
